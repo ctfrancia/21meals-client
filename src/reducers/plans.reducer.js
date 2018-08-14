1efb8e336446004c68bd5edfc0a16c74af157943
Jordi M. Zambrano <nickschoey@gmail.com>
2018-08-14 12:06:57 +0200

    second reducer for shopping list0

--- src/reducers/plans.reducer.js
+++ src/reducers/plans.reducer.js
@@ -1,5 +1,18 @@
-import { plansConstants } from '../constants/plans.constants';
-const defaultState = { plans: [], plan: {monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: []}, shoppingList: [], loading: true };
+import { plansConstants } from "../constants/plans.constants";
+const defaultState = {
+  plans: [],
+  plan: {
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: [],
+    sunday: []
+  },
+  shoppingList: [],
+  loading: true
+};
 
 export default (state = defaultState, action) => {
   switch (action.type) {
@@ -7,28 +20,49 @@ export default (state = defaultState, action) => {
       return {
         ...state,
         loading: true
-      }
+      };
 
     case plansConstants.PLANS_GET_ALL_SUCCESS:
       return {
         ...state,
-        plan: action.data[0].meals.reduce((acc,el) => {
+        plan: action.data[0].meals.reduce((acc, el) => {
           acc[el.weekday] = el.recipe;
-          return acc
+          return acc;
         }, {}),
         plans: action.data[0],
         loading: false,
-        shoppingList: action.data[0].meals.reduce((acc, el) => {
-          if (el.recipe !== null) {
-            console.log(el.recipe);
-            
-            el.recipe.ingredients.forEach(ingredient => acc.push(ingredient))
-          }
-          return acc;
+        shoppingList: 
+        action.data[0].meals
+        //This reduce extracts all the ingredients from all the recipes in the plan
+          .reduce((acc, el) => {
+            if (el.recipe !== null) {
+              el.recipe.ingredients.forEach(ingredient => acc.push(ingredient));
+            }
+            return acc;
           }, [])
-        }
+          // this reduce splits all ingredients and adds up the quantitites
+          .reduce((acc, el) => {
+            if (!acc.hasOwnProperty(el.ingredient.name)) {
+              acc[el.ingredient.name] = {
+                amount: el.amount,
+                measure: el.measure,
+                type: el.ingredient.ingredient_type
+              };
+            } else {
+              acc[el.ingredient.name] = {
+                ...acc[el.ingredient.name],
+                amount: el.amount + acc[el.ingredient.name].amount
+              }
+            }
+            return acc;
+          }, {})
+          // Split them into types
+          // .reduce((acc, el) => {
+          
+          // }, {})
+      };
 
     default:
       return state;
   }
-}
+};
