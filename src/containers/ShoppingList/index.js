5ff5e2b245981360976871a633e11262b073ac2c
Jordi M. Zambrano <nickschoey@gmail.com>
2018-08-15 21:06:06 +0200

    shopping list forms dynamically!

--- src/containers/ShoppingList/index.js
+++ src/containers/ShoppingList/index.js
@@ -25,13 +25,40 @@ function onChange(e) {
 }
 
 class ShoppingList extends React.Component {
-  
-  componentDidMount = () => {
-    
+  createShoppingList() {
+    let list = [];
+    for (const category in this.props.listItems) {
+      if (this.props.listItems.hasOwnProperty(category)) {
+        list.push(
+          <List
+            key={category}
+            header={
+              <div className="list__list--header">
+                <Icon type="coffee" />
+                {category.toUpperCase()}
+              </div>
+            }
+            bordered
+            dataSource={this.props.listItems[category]}
+            renderItem={item => (
+              <List.Item key={item.id}>
+                <Checkbox onChange={onChange}>
+                  {`${item.amount === 0 || item.amount === null ? 'Some' : item.amount} ${item.measure === null ? '': item.measure} ${item.name}`}
+                </Checkbox>
+              </List.Item>
+            )}
+          />
+        );
+      }
+    }
+    return list;
   }
-  
+
+  componentDidMount = () => {};
+
   render() {
     console.log(this.props.listItems);
+    // console.log(this.props.ingredients_recipe);
 
     return (
       <div>
@@ -43,68 +70,7 @@ class ShoppingList extends React.Component {
                   <h2>Shopping List</h2>
                 </div>
               </div>
-              <div className="list__list">
-                <List
-                  header={
-                    <div className="list__list--header">
-                      <Icon type="coffee" />
-                      Drinks
-                    </div>
-                  }
-                  bordered
-                  dataSource={drinks}
-                  renderItem={item => (
-                    <List.Item>
-                      <Checkbox onChange={onChange}>{item}</Checkbox>
-                    </List.Item>
-                  )}
-                />
-                <List
-                  header={
-                    <div className="list__list--header">
-                      <Icon type="coffee" />
-                      Meat
-                    </div>
-                  }
-                  bordered
-                  dataSource={meat}
-                  renderItem={item => (
-                    <List.Item>
-                      <Checkbox onChange={onChange}>{item}</Checkbox>
-                    </List.Item>
-                  )}
-                />
-                <List
-                  header={
-                    <div className="list__list--header">
-                      <Icon type="coffee" />
-                      Drinks
-                    </div>
-                  }
-                  bordered
-                  dataSource={drinks}
-                  renderItem={item => (
-                    <List.Item>
-                      <Checkbox onChange={onChange}>{item}</Checkbox>
-                    </List.Item>
-                  )}
-                />
-                <List
-                  header={
-                    <div className="list__list--header">
-                      <Icon type="coffee" />
-                      Meat
-                    </div>
-                  }
-                  bordered
-                  dataSource={meat}
-                  renderItem={item => (
-                    <List.Item>
-                      <Checkbox onChange={onChange}>{item}</Checkbox>
-                    </List.Item>
-                  )}
-                />
-              </div>
+              <div className="list__list">{this.createShoppingList()}</div>
             </div>
           </Content>
         </Layout>
@@ -116,12 +82,67 @@ class ShoppingList extends React.Component {
 ShoppingList.propTypes = {};
 
 const mapStateToProps = state => ({
-  listItems: Object.keys(state.entities.meals_plan).map(el => {
-    const recipeIngredients = state.entities.recipes[state.entities.meals_plan[el].recipe_id]
-    return recipeIngredients;
-  })
+  ingredients_recipe: state.entities.ingredients_recipe,
+
+  listItems: Object.values(
+    Object.values(state.entities.meals_plan)
+      .reduce((acc, el) => {
+        if (el.recipe_id) acc.push(el.recipe_id);
+        return acc;
+      }, []) //Gets where there's a recipe
+      .map(el => state.entities.recipes[el].ingredients) //gets ingredients/measures references from recipe
+      .reduce((acc, el) => acc.concat(el), []) // Flatten array
+      .reduce((acc, el) => {
+        //gets actual ingredients/measures
+        acc.push(state.entities.ingredients_recipe[el]);
+        return acc;
+      }, [])
+      .reduce((acc, el) => {
+        //gets name/type related with ingredient
+        acc.push({
+          id: el.ingredient_id,
+          name: state.entities.allIngredients[el.ingredient_id].name,
+          type: state.entities.allIngredients[el.ingredient_id].ingredient_type,
+          amount: el.amount,
+          measure: el.measure
+        });
+        return acc;
+      }, [])
+      .reduce((acc, el) => {
+        if (!acc.hasOwnProperty(el.id)) {
+          acc[el.id] = el;
+        } else {
+          acc[el.id].amount += el.amount;
+        }
+        return acc;
+      }, {})
+  ).reduce((acc, el) => {
+    if (!acc.hasOwnProperty(el.type)) {
+      acc[el.type] = [el];
+    } else {
+      acc[el.type].push(el);
+    }
+    return acc;
+  }, {})
 });
 
+// .reduce((acc, el) => {
+//   if (el.recipe_id) {
+//     const recipeIngredients = state.entities.recipes[el.recipe_id].ingredients //full list of ingredients/amount references
+//     const fullList = recipeIngredients.map(el => {
+//       //switch references for actual data
+//       return state.entities.ingredients_recipe[el]
+
+//       // return {
+//       //   name: state.entities.allIngredients[el.ingredient_id].name,
+//       //   amount: el.amount
+//       // }
+//     })
+//     acc.push(fullList)
+
+//     return acc
+//   }
+// }, [])
 const mapDispatchToProps = dispatch => ({});
 //Generate list of meals from meals_plan [1,2,1]
 //every recipe yelds some ingredients_recipe [1...9, 10...18]
