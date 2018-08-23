77792d0fd489f61a698b5f98ed900dcc42bb32e1
Jordi M. Zambrano <nickschoey@gmail.com>
2018-08-23 16:04:31 +0200

    API?

--- src/components/GlobalCard/index.js
+++ src/components/GlobalCard/index.js
@@ -1,25 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Card, Modal, Tabs, Input, Form, Select, InputNumber, Icon, Button } from 'antd';
-import { getOneRecipe, removeIngredientFromGlobal, postRecipe } from '../../actions/recipes.actions';
+import {
+  Card,
+  Modal,
+  Tabs,
+  Input,
+  Form,
+  Select,
+  InputNumber,
+  Divider,
+  Button
+} from 'antd';
+import {
+  getOneRecipe,
+  removeIngredientFromGlobal,
+  postRecipe
+} from '../../actions/recipes.actions';
 import { postIngredient } from '../../actions/ingredients.actions';
 
 import './index.css';
 
-const InputGroup = Input.Group;
 const Option = Select.Option;
 const TabPane = Tabs.TabPane;
 const FormItem = Form.Item;
-const {Meta} = Card;
 const styles = {
   card: {
     width: 155,
     borderRadius: 15
   },
   img: {
-    height: 130,
-    borderRadius: '15px ',
+    height: 75,
+    width: 105,
+    borderRadius: '5px ',
     objectFit: 'cover'
   },
   modalImg: {
@@ -46,17 +59,17 @@ class GlobalCard extends Component {
         ingredient_id: '',
         amount: '',
         measure: 'units',
-        measure_id: 9,
+        measure_id: 9
       },
       newIngredient: {
         name: '',
-        ingredient_type_id: 1,
+        ingredient_type_id: 1
       },
       tabs: {
         activeKey: '1',
         next: true,
         prev: false
-      },
+      }
     };
   }
 
@@ -67,7 +80,7 @@ class GlobalCard extends Component {
       recipe: {
         ...this.state.recipe,
         title: this.props.title,
-        photo: this.props.imageUrl,
+        photo: this.props.imageUrl
       }
     });
   }
@@ -167,232 +180,268 @@ class GlobalCard extends Component {
 
   ingredientTypeSelect = () => {
     return Object.values(this.props.ingredientTypes).map((el, i) => {
-          return (
-            <Option key={i} value={el.id}>
-              {el.name}
-            </Option>
-       );
+      return (
+        <Option key={i} value={el.id}>
+          {el.name}
+        </Option>
+      );
     });
   };
 
   ingredientListMap = () => {
     return this.state.recipe.ingredients.map((el, i) => {
-          return (
-            <li key={i} value={el.id}>
-              {el.title} {el.amount} {el.measure}
-            </li>
-       );
+      return (
+        <li key={i} value={el.id}>
+          {el.title} {el.amount} {el.measure}
+        </li>
+      );
     });
-  }
+  };
 
   aproveIngredient = async (globalID, globalIngredientName, e) => {
     e.preventDefault();
     let globalIngredientID = 'amount' + globalID;
     for (let i = 1; i <= Object.keys(this.props.ingredients).length; i++) {
-      if (e.target[globalIngredientName].value === this.props.ingredients[i].name) {
+      if (
+        e.target[globalIngredientName].value === this.props.ingredients[i].name
+      ) {
         await this.setState({
           ...this.state,
           selectedIngredient: {
             ...this.state.selectedIngredient,
             title: this.props.ingredients[i].name,
             ingredient_id: this.props.ingredients[i].id,
-            amount: e.target[globalIngredientID].value,
-          },
+            amount: e.target[globalIngredientID].value
+          }
         });
         break;
       }
     }
     if (this.state.selectedIngredient.title !== '') {
-      this.setState({
-        ...this.state,
-        recipe: {
-          ...this.state.recipe,
-          instructions: this.props.recipeDetails.Instructions,
-          ingredients: [ ...this.state.recipe.ingredients, {...this.state.selectedIngredient} ],
-          title: this.props.name,
-          photo: this.props.imageUrl,
-        },
-      }, () => {
-        this.setState({
-          ...this.state,
-          newIngredient: {
-            ingredient_type_id: 1,
-            name: '',
-          },
-          selectedIngredient: {
-            ...this.state.selectedIngredient,
-            title: '',
-            ingredient_id: '',
-            amount: '',
-          },
-        })
-      });
-    } else {
-      this.setState({
-        ...this.state,
-        newIngredient: {
-          ...this.state.newIngredient,
-          name: e.target[globalIngredientName].value,
-        },
-        selectedIngredient: {
-          ...this.state.selectedIngredient,
-          title: e.target[globalIngredientName].value,
-          ingredient_id: Object.keys(this.props.ingredients).length+1,
-          amount: e.target[globalIngredientID].value,
-        },
-      }, () => {
-        this.props.postIngredient(this.state.newIngredient);
-        this.setState({
+      this.setState(
+        {
           ...this.state,
           recipe: {
             ...this.state.recipe,
             instructions: this.props.recipeDetails.Instructions,
-            ingredients: [ ...this.state.recipe.ingredients, {...this.state.selectedIngredient} ],
+            ingredients: [
+              ...this.state.recipe.ingredients,
+              { ...this.state.selectedIngredient }
+            ],
             title: this.props.name,
-            photo: this.props.imageUrl,
-          },
-        }, () => {
+            photo: this.props.imageUrl
+          }
+        },
+        () => {
           this.setState({
             ...this.state,
             newIngredient: {
               ingredient_type_id: 1,
-              name: '',
+              name: ''
             },
             selectedIngredient: {
               ...this.state.selectedIngredient,
               title: '',
               ingredient_id: '',
-              amount: '',
+              amount: ''
+            }
+          });
+        }
+      );
+    } else {
+      this.setState(
+        {
+          ...this.state,
+          newIngredient: {
+            ...this.state.newIngredient,
+            name: e.target[globalIngredientName].value
+          },
+          selectedIngredient: {
+            ...this.state.selectedIngredient,
+            title: e.target[globalIngredientName].value,
+            ingredient_id: Object.keys(this.props.ingredients).length + 1,
+            amount: e.target[globalIngredientID].value
+          }
+        },
+        () => {
+          this.props.postIngredient(this.state.newIngredient);
+          this.setState(
+            {
+              ...this.state,
+              recipe: {
+                ...this.state.recipe,
+                instructions: this.props.recipeDetails.Instructions,
+                ingredients: [
+                  ...this.state.recipe.ingredients,
+                  { ...this.state.selectedIngredient }
+                ],
+                title: this.props.name,
+                photo: this.props.imageUrl
+              }
             },
-          })
-        })
-      });
+            () => {
+              this.setState({
+                ...this.state,
+                newIngredient: {
+                  ingredient_type_id: 1,
+                  name: ''
+                },
+                selectedIngredient: {
+                  ...this.state.selectedIngredient,
+                  title: '',
+                  ingredient_id: '',
+                  amount: ''
+                }
+              });
+            }
+          );
+        }
+      );
     }
     if (this.props.recipeDetails.Ingredients.length === 1) this.changeTab(true);
     this.props.removeIngredientFromGlobal(globalID);
-  }
+  };
 
-  ingredientTypeChange = (value) => {
-    this.setState( {
+  ingredientTypeChange = value => {
+    this.setState({
       ...this.state,
       newIngredient: {
         ...this.state.newIngredient,
-        ingredient_type_id: value,
+        ingredient_type_id: value
       }
-    })
-  }
+    });
+  };
 
   createNewRecipe = () => {
     this.props.postRecipe(this.state.recipe);
     this.handleOk();
-  }
+  };
 
-  ingredientMeasureChange = (value) => {
+  ingredientMeasureChange = value => {
     let measure;
-    for (let i = 1; i <= Object.keys(this.props.measures).length; i++) if (value === this.props.measures[i].id) measure = this.props.measures[i].name;
-    this.setState( {
+    for (let i = 1; i <= Object.keys(this.props.measures).length; i++)
+      if (value === this.props.measures[i].id)
+        measure = this.props.measures[i].name;
+    this.setState({
       ...this.state,
       selectedIngredient: {
         ...this.state.selectedIngredient,
         measure_id: value,
-        measure: measure,
+        measure: measure
       }
-    })
-  }
-
+    });
+  };
 
   render() {
-    return (<div className="recipe_card">
-      <Card hoverable="hoverable" onClick={this.showModal.bind(this, this.props.id)} style={styles.card} cover={<img
-        alt = {
-          this.props.name
-        }
-        src = {
-          this.props.imageUrl
-        }
-        style = {
-          styles.img
-        }
-        />}>
-        <Meta title={this.props.name}/>
-      </Card>
-      <Modal title={this.props.name} visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel} footer={[
-        <Button
-          disabled={!this.state.tabs.prev ? true : false}
-          key="back"
-          size="large"
-          type="primary"
-          onClick={this.changeTab.bind(this, false)}
+    return (
+      <div className="recipe_card">
+        <div className="recipe_card--main" onClick={this.showModal.bind(this, this.props.id)}>
+            <div className="recipe_card--info">
+              <h2>{this.props.name}</h2>
+            </div>
+            <div className="recipe_card--image">
+              <img alt={this.props.name} src={this.props.imageUrl} style={styles.img} />
+            </div>
+          </div>
+        <Modal
+          title={this.props.name}
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={[
+            <Button
+              disabled={!this.state.tabs.prev ? true : false}
+              key="back"
+              size="large"
+              type="primary"
+              onClick={this.changeTab.bind(this, false)}
+            >
+              Prev
+            </Button>,
+            <Button
+              disabled={!this.state.tabs.next ? true : false}
+              key="submit"
+              type="primary"
+              size="large"
+              onClick={this.changeTab.bind(this, true)}
+            >
+              Next
+            </Button>
+          ]}
         >
-          Prev
-        </Button>,
-        <Button
-          disabled={!this.state.tabs.next ? true : false}
-          key="submit"
-          type="primary"
-          size="large"
-          onClick={this.changeTab.bind(this, true)}
-        >
-          Next
-        </Button>
-      ]}>
-        <Tabs tabPosition="top" size="small" activeKey={this.state.tabs.activeKey} onTabClick={this.handleTabClick}
-        >
-          <TabPane tab="Recipe" key="1">
-            <img alt={this.props.name} src={this.props.imageUrl} style={styles.modalImg}/>
-            <p>{this.props.recipeDetails.Instructions}</p>
-          </TabPane>
-          <TabPane tab="Validate Ingredients" key="2">
-            <div>
-                {
-                  this.props.recipeDetails.Ingredients.map((el, i) => {
-                    if (el.Name) {
-                      return (<Form key={this.props.recipeDetails.Ingredients.length - i} onSubmit={this.aproveIngredient.bind(this, el.IngredientID, el.Name)}> <FormItem key={i} label={`${el.Quantity} ${el.Unit} ${el.Name}`} >
-                      <div className="InputGroup">
-                        <InputGroup compact="compact">
-                          <Input style={{
-                              width: '35%',
-                              textAlign: 'left'
-                            }} defaultValue={el.Name} name={el.Name} />
-                            <Select defaultValue={1} style={{
-                                width: '25%',
-                              }} onChange={this.ingredientTypeChange}>
-                              {this.ingredientTypeSelect()}
-                            </Select>
-                            <Select defaultValue={9} style={{
-                                width: '25%',
-                              }} onChange={this.ingredientMeasureChange}>
+          <Tabs
+            tabPosition="top"
+            size="small"
+            activeKey={this.state.tabs.activeKey}
+            onTabClick={this.handleTabClick}
+          >
+            <TabPane tab="Recipe" key="1">
+              <img
+                alt={this.props.name}
+                src={this.props.imageUrl}
+                style={styles.modalImg}
+              />
+              <h3>{this.props.recipeDetails.Instructions}</h3>
+            </TabPane>
+            <TabPane tab="Validate Ingredients" key="2">
+              <div>
+                {this.props.recipeDetails.Ingredients.map((el, i) => {
+                  if (el.Name) {
+                    return <Form key={i} onSubmit={this.aproveIngredient.bind(this, el.IngredientID, el.Name)}>
+                      <Divider>{`${el.Quantity} ${el.Unit} ${el.Name}`}</Divider>
+                        <FormItem label="1. Verify ingredient's name">
+                          <Input style={{ textAlign: 'left' }} defaultValue={el.Name} name={el.Name} />
+                        </FormItem>
+                        <div className="globalAmounts">
+                          <FormItem label="2. Amount">
+                            <InputNumber defaultValue={el.Quantity} name={'amount' + el.IngredientID}  />
+                          </FormItem>
+                          <FormItem label="3. Unit">
+                            <Select defaultValue={9} onChange={this.ingredientMeasureChange}>
                               {this.measureSelect()}
                             </Select>
-                            <InputNumber defaultValue={el.Quantity} name={'amount' + el.IngredientID} style={{
-                                width: '15%',
-                              }}/>
+                          </FormItem>
+                          <FormItem label="4. Category">
+                            <Select defaultValue={1} onChange={this.ingredientTypeChange}>
+                              {this.ingredientTypeSelect()}
+                            </Select>
+                          </FormItem>
+                        </div>
 
-                        </InputGroup>
-                        <div className="accept__decline" >
+                        <div className="accept__decline">
                           <div>
-                            <Button className="accept" htmlType="submit" icon="check">Accept Ingredient</Button>
+                            <Button type="primary" className="accept" htmlType="submit" icon="check">
+                              Accept
+                            </Button>
                           </div>
                           <div>
-                            <Button className="decline" icon="close" onClick={this.props.removeIngredientFromGlobal.bind(this, el.IngredientID)} >Remove Ingredient</Button>
+                            <Button type="danger" className="decline" icon="close" onClick={this.props.removeIngredientFromGlobal.bind(this, el.IngredientID)}>
+                              Remove
+                            </Button>
                           </div>
                         </div>
-                        </div>
-                      </FormItem> </Form>);
-                    }
-                  })
-                }
-            </div>
-          </TabPane>
-          <TabPane tab="Review" key="3">
-            <img alt={this.props.name} src={this.props.imageUrl} style={styles.modalImg}/>
-            <p>{this.props.recipeDetails.Instructions}</p>
-            <ul>{this.ingredientListMap()}</ul>
-            <Button onClick={this.createNewRecipe.bind(this)} >Add to my recipes</Button>
-          </TabPane>
-        </Tabs>
-      </Modal>
-    </div>);
+                      <Divider></Divider>
+                      </Form>
+                  }
+                })}
+              </div>
+            </TabPane>
+            <TabPane tab="Review" key="3">
+              <img
+                alt={this.props.name}
+                src={this.props.imageUrl}
+                style={styles.modalImg}
+              />
+              <p>{this.props.recipeDetails.Instructions}</p>
+              <ul>{this.ingredientListMap()}</ul>
+              <Button onClick={this.createNewRecipe.bind(this)}>
+                Add to my recipes
+              </Button>
+            </TabPane>
+          </Tabs>
+        </Modal>
+      </div>
+    );
   }
 }
 
@@ -412,21 +461,25 @@ GlobalCard.propTypes = {
   removeIngredientFromGlobal: PropTypes.func,
   photo: PropTypes.string,
   postIngredient: PropTypes.func,
-  postRecipe: PropTypes.func,
+  postRecipe: PropTypes.func
 };
 
 const mapStateToProps = state => ({
   recipeDetails: state.pages.globalRecipe,
   measures: state.entities.measures,
   ingredientTypes: state.entities.ingredient_types,
-  ingredients: state.entities.allIngredients,
+  ingredients: state.entities.allIngredients
 });
 
 const mapDispatchToProps = dispatch => ({
   getOneRecipe: RecipeID => dispatch(getOneRecipe(RecipeID)),
-  removeIngredientFromGlobal: ingredient => dispatch(removeIngredientFromGlobal(ingredient)),
+  removeIngredientFromGlobal: ingredient =>
+    dispatch(removeIngredientFromGlobal(ingredient)),
   postIngredient: data => dispatch(postIngredient(data)),
-  postRecipe: data => dispatch(postRecipe(data)),
+  postRecipe: data => dispatch(postRecipe(data))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(GlobalCard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GlobalCard);
